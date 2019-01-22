27aa9343f1fe7ae63fbcca95a257645cda04f707
bnfour <bnfour@bnfour.net>
2019-01-22 13:50:23 +0700

    result strings (and temporary link) added to const file

--- const.py
+++ const.py
@@ -1,5 +1,17 @@
 # various constants used throughout
 MUTE = -1
+
 # relative icon paths
 ICON = "icons/icon.png"
 ICON_ERROR = "icons/error.png"
+
+# strings to populate results with
+MUTE_TITLE = "Toggle mute status"
+# have no idea what to write here ¯\_(ツ)_/¯
+MUTE_SUBTITLE = "Just in case"
+# {} is to insert actual volume via format
+SET_VOLUME_TITLE = "Set volume to {}"
+SET_VOLUME_SUBTITLE = "This also unmutes"
+ERROR_TITLE = "Additional setup required!"
+ERROR_SUBTITLE = "Some required libraries are missing. Select to learn more"
+ERROR_URL = "https://todo.lul/"
